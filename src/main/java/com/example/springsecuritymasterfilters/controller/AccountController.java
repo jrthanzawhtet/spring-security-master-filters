89cc1e34f6af241916730e78c73f66f1fed452a1
jrthanzawhtet <thanzawhtet18@gmail.com>
2024-01-10 12:01:22 +0630

    firstcommit

---
+++ src/main/java/com/example/springsecuritymasterfilters/controller/AccountController.java
@@ -0,0 +1,24 @@
+package com.example.springsecuritymasterfilters.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/login-error")
+    public String loginError(Model model){
+        model.addAttribute("loginError",true);
+        return "login";
+    }
+
+
+
+}
